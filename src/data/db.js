8ea5726fc37b4c81a00db6e1c5b6fa031f92c57f
Prisci <pr.rodriguezr@duocuc.cl>
2024-05-22 20:14:13 -0400

    primer commit con estructura a la rama principal

---
+++ src/data/db.js
@@ -0,0 +1,15 @@
+export const db = [ 
+    {
+        email: 'pr.rodriguezr',
+        nombre: 'priscila',
+        pass: 'frijolito2000'
+    },
+    {
+        email: 'fjfrfrujf@ergmk',
+        nombre: 'frijolito',
+        pass: 'olaolaola'
+    }
+
+]
+
+
