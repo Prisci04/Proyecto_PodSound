689f292fe0fe01d321851f011eebca743304ba0a
Álvaro Andrés Castillo Rodriguez <alvaro.castillo@kobai.cl>
2024-06-04 03:32:05 -0400

    1

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
 //url de la api
-const API = 'http://localhost:3000/api'
+const API = 'https://us-central1-podsound-fb86e.cloudfunctions.net/api_backend_podsound/api'
 
 
 //esta funcion toma como objeto el user como argumento y realiza una peticion post a la url
 export const registerRequest = user => axios.post(`${API}/register`, user);
 
+
+
+//esta funcion toma como objeto el user como argumento y realiza una peticion post a la url
+export const loginRequest = user => axios.post(`${API}/login`, user);
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -11,13 +11,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import  AppRouter from './Router'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>,
-)
