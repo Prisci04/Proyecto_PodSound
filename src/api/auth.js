d8eace0a6e377bb5ca289fd1ee26606056a5f202
Álvaro Andrés Castillo Rodriguez <alvaro.castillo@kobai.cl>
2024-06-04 21:25:25 -0400

    2

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //url de la api
-const API = 'https://us-central1-podsound-fb86e.cloudfunctions.net/api_backend_podsound/api'
+const API = 'http://localhost:3000/api'
 
 
 //esta funcion toma como objeto el user como argumento y realiza una peticion post a la url
